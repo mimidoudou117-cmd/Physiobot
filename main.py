b5e7ab6120af7649e7230dbbfd377a3b7478d14e
mimidoudou117-cmd <mimidoudou117@gmail.com>
2026-08-15 20:26:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 # تهيئة Supabase و Gemini
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
+model = genai.GenerativeModel('gemini-1.5-flash')
+
 
 
 
